940d8716129241f947bbd96867981a59f06fed7b
Richie Parks <parksrichiework@gmail.com>
2023-03-06 16:59:30 -0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,50 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// Require Express to run server and routes
+const express = require('express')
+
+// Start up an instance of app
+const app = express()
+
+//Dependencies
+const bodyParser = require('body-parser')
+
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+
+const cors = require('cors')
+app.use(cors())
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+const port = 3000
+
+// Setup Server
+
+const server = app.listen(port, listening)
+
+function listening(err){
+    if(err){
+        return console.log('ERROR', err)
+    }
+    console.log(server)
+    console.log(`Listening on localhost: ${port}`)
+}
+
+//GET route that returns project data
+// '/' = root or "home"
+app.get('/', function(req,res){
+    res.send(projectData)
+})
+
+//POST route that adds incoming data to project data
+app.post('/', function(req,res){
+    res.send({"temperature": '', "date": '', "userResponse": ''})
+})
